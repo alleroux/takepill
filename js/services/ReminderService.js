d3e3c837fcde570a083731a7ad9a748a104480cb
Alexander Leroux <alexander.l.leroux@gmail.com>
2014-11-09 19:39:54 -0800

    Added timeout functions.

--- js/services/ReminderService.js
+++ js/services/ReminderService.js
@@ -9,7 +9,24 @@ takePillApp.factory('ReminderService', ['$timeout', function($timeout) {
         reminders : [],
         set : function(interval, start, count) {
             reminders.push(new reminder(interval, start, count));
+            this.setTimeouts(this.getIntervalMilliseconds(interval, start, count));
             this.serialize();
+        },
+        setTimeouts : function(intervals) {
+            for(var i = 0; i < intervals.length; i++) {
+                $timeout(this.alarm, intervals[i]);
+            }
+        },
+        getIntervalMilliseconds : function(interval, start, count) {
+            var startMilliseconds = start.getTime();
+            var intervalMilliseconds = interval * 60 * 60 * 1000;
+            var intervals = [];
+            var nextAlarmTime = startMilliseconds;
+            for(var i = 0; i < count; i++) {
+                nextAlarmTime = nextAlarmTime + intervalMilliseconds;
+                intervals.push(nextAlarmTime);
+            }
+            return intervals;
         },
         serialize : function() {
             localStorage.set('reminders', JSON.stringify(this.reminders));
